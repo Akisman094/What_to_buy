c12c45701d5c99946846e5448cf2e8851fc8b1bb
Akmal Ismoilov <master80098@gmail.com>
2023-03-26 23:29:30 +0500

    Added FamilyService Models

---
+++ Services/WhatToBuy.Services.Families/Models/FamilyModel.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using WhatToBuy.Context.Entities;
+
+namespace WhatToBuy.Services.Families;
+
+public class FamilyModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public ICollection<string> Users { get; set; }
+
+    public ICollection<string> ShoppingLists { get; set; }
+}
+
+public class FamilyResponseDtoProfile : Profile
+{
+    public FamilyResponseDtoProfile()
+    {
+        CreateMap<Family, FamilyModel>()
+            .AfterMap((s, d) =>
+            {
+                d.Users = new List<string>(s.Users.Select(x => x.Name));
+                d.ShoppingLists = new List<string>(s.ShoppingLists.Select(x => x.Name));
+            });
+    }
+}
---
+++ Services/WhatToBuy.Services.Families/Models/FamilyUpdateModel.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using WhatToBuy.Context.Entities;
+
+namespace WhatToBuy.Services.Families;
+
+public class FamilyUpdateModel
+{
+    public string Name { get; set; }
+}
+
+public class FamilyUpdateModelProfile : Profile
+{
+    public FamilyUpdateModelProfile()
+    {
+        CreateMap<FamilyUpdateModel, Family>();
+    }
+}
